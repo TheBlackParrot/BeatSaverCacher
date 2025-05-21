5817afe5882b8bbd7651414d32c02085660c56c8
TheBlackParrot <theblackparrot@rocketmail.com>
2025-05-21 15:25:37 -0500

    initial commit, still wip probly

---
+++ README.md
@@ -0,0 +1,4 @@
+still wip  
+will eventually use this to replace a dependency on SongDetailsCache in the DumbRequestManager mod
+
+note to self: `protoc --proto_path=. --js_out=import_style=commonjs,binary:. mapData.proto`
---
+++ main.js
@@ -0,0 +1,118 @@
+const MapListSchema = require("./mapData_pb.js");
+const fs = require("fs");
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const BeatSaverAPI = "https://api.beatsaver.com/maps/latest";
+
+function doesMapUseMod(mapData, modName) {
+    for(const diff of mapData.diffs) {
+        if(modName in diff) {
+            if(diff[modName] === true) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
+
+async function run() {
+    let currentDateString = new Date().toISOString();
+    let keepGoing = true;
+    let amount = 0;
+
+    let mapList = new MapListSchema.MapList();
+    
+    while(keepGoing) {
+        let params = {
+            automapper: false,
+            before: currentDateString,
+            pageSize: 100,
+            sort: "CREATED"
+        }
+        let searchParams = new URLSearchParams(params);
+
+        const URL = `${BeatSaverAPI}?${searchParams}`;
+
+        const response = await fetch(URL);
+        if(response.status !== 200) {
+            console.log("Status not 200, waiting");
+            await delay(3000);
+            continue;
+        }
+        
+        const data = await response.json();
+
+        for(const mapData of data.docs) {
+            amount++;
+            
+            let entry = new MapListSchema.MapMetadata();
+            entry.setKey(parseInt(mapData.id, 16));
+            entry.setHash(mapData.versions[0].hash);
+            entry.setDuration(mapData.metadata.duration);
+            entry.setUploaded(Math.round(new Date(mapData.lastPublishedAt).getTime() / 1000));
+            entry.setLastupdated(Math.round(new Date(mapData.updatedAt).getTime() / 1000));
+            entry.setMods((doesMapUseMod(mapData.versions[0], "cinema") ? MapListSchema.MapMods.CINEMA : 0) +
+                (doesMapUseMod(mapData.versions[0], "me") ? MapListSchema.MapMods.MAPPINGEXTENSIONS : 0) +
+                (doesMapUseMod(mapData.versions[0], "chroma") ? MapListSchema.MapMods.CHROMA : 0) +
+                (doesMapUseMod(mapData.versions[0], "ne") ? MapListSchema.MapMods.NOODLEEXTENSIONS : 0) +
+                (doesMapUseMod(mapData.versions[0], "vivify") ? MapListSchema.MapMods.VIVIFY : 0));
+            
+            let votes = new MapListSchema.Votes();
+            votes.setUp(mapData.stats.upvotes);
+            votes.setDown(mapData.stats.upvotes);
+            entry.setVotes(votes);
+
+            if("curator" in mapData) { entry.setCuratorname(mapData.curator.name); }
+            if(mapData.metadata.songName !== "") { entry.setSongname(mapData.metadata.songName); }
+            if(mapData.metadata.songSubName !== "") { entry.setSongsubname(mapData.metadata.songSubName); }
+            if(mapData.metadata.songAuthorName !== "") { entry.setSongauthorname(mapData.metadata.songAuthorName); }
+            if(mapData.metadata.levelAuthorName !== "") { entry.setLevelauthorname(mapData.metadata.levelAuthorName); }
+            
+            for(const diff of mapData.versions[0].diffs) {
+                let diffEntry = new MapListSchema.Difficulty();
+                diffEntry.setNjs(diff.njs);
+                diffEntry.setNotes(diff.notes);
+                diffEntry.setCharacteristicname(diff.characteristic);
+                diffEntry.setDifficultyname(diff.difficulty);
+                diffEntry.setMods((diff.cinema ? MapListSchema.MapMods.CINEMA : 0) +
+                    (diff.me ? MapListSchema.MapMods.MAPPINGEXTENSIONS : 0) +
+                    (diff.chroma ? MapListSchema.MapMods.CHROMA : 0) +
+                    (diff.ne ? MapListSchema.MapMods.NOODLEEXTENSIONS : 0) +
+                    (diff.vivify ? MapListSchema.MapMods.VIVIFY : 0));
+                diffEntry.setEnvironmentname(diff.environment.replaceAll("Environment", ""));
+                
+                entry.addDifficulties(diffEntry);
+            }
+            
+            mapList.addMapmetadata(entry);
+        }
+        
+        console.log(`Cached ${amount} maps...`);
+        
+        if(!data.docs.length) {
+            console.log("No maps left!");
+            keepGoing = false;
+        } else {
+            currentDateString = data.docs[data.docs.length - 1].uploaded;
+            await delay(100);
+        }
+    }
+    
+    fs.writeFileSync("./cached.proto", mapList.serializeBinary());
+    
+    //fs.writeFileSync("./test.json", JSON.stringify(MapListSchema.MapList.deserializeBinary(mapList.serializeBinary()).toObject(), null, "\t"));
+}
+/*
+let root = protobuf.load("./mapData.proto").then(function(proto) {
+    let MessageType = proto.lookupType("CachedBeatSaverData.MapMetadata");
+    let payload = { key: 0x1af };
+
+    let errMsg = MessageType.verify(payload);
+    if (errMsg)
+        throw Error(errMsg); 
+});
+//run();
+*/
+run();
