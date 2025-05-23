f87308e6ead901f650ce7b18a4dc31f04de77c86
TheBlackParrot <theblackparrot@rocketmail.com>
2025-05-23 12:10:08 -0500

    ok well stuff works, it's just asserting everywhere

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const MapListSchema = require("./mapData_pb.js");
-const fs = require("fs");
+import MapListSchema from "./mapData_pb.js";
+import fs from "fs";
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const BeatSaverAPI = "https://api.beatsaver.com/maps/latest";
+const BeatSaverSocketURL = "wss://ws.beatsaver.com/maps";
 
 function doesMapUseMod(mapData, modName) {
     for(const diff of mapData.diffs) {
@@ -17,13 +18,13 @@ function doesMapUseMod(mapData, modName) {
     return false;
 }
 
-async function run() {
+let mapList = new MapListSchema.MapList();
+let mapListMap = mapList.getMapmetadataMap();
+
+async function initCache() {
     let currentDateString = new Date().toISOString();
     let keepGoing = true;
     let amount = 0;
-
-    let mapList = new MapListSchema.MapList();
-    let mapListMap = mapList.getMapmetadataMap();
     
     while(keepGoing) {
         let params = {
@@ -48,62 +49,140 @@ async function run() {
         for(const mapData of data.docs) {
             amount++;
             
-            let entry = new MapListSchema.MapMetadata();
-            entry.setKey(parseInt(mapData.id, 16));
-            entry.setHash(mapData.versions[0].hash);
-            entry.setDuration(mapData.metadata.duration);
-            entry.setUploaded(Math.round(new Date(mapData.lastPublishedAt).getTime() / 1000));
-            entry.setLastupdated(Math.round(new Date(mapData.updatedAt).getTime() / 1000));
-            entry.setMods((doesMapUseMod(mapData.versions[0], "cinema") ? MapListSchema.MapMods.CINEMA : 0) +
-                (doesMapUseMod(mapData.versions[0], "me") ? MapListSchema.MapMods.MAPPINGEXTENSIONS : 0) +
-                (doesMapUseMod(mapData.versions[0], "chroma") ? MapListSchema.MapMods.CHROMA : 0) +
-                (doesMapUseMod(mapData.versions[0], "ne") ? MapListSchema.MapMods.NOODLEEXTENSIONS : 0) +
-                (doesMapUseMod(mapData.versions[0], "vivify") ? MapListSchema.MapMods.VIVIFY : 0));
-            
-            let votes = new MapListSchema.Votes();
-            votes.setUp(mapData.stats.upvotes);
-            votes.setDown(mapData.stats.downvotes);
-            entry.setVotes(votes);
-
-            if("curator" in mapData) { entry.setCuratorname(mapData.curator.name); }
-            if(mapData.metadata.songName !== "") { entry.setSongname(mapData.metadata.songName); }
-            if(mapData.metadata.songSubName !== "") { entry.setSongsubname(mapData.metadata.songSubName); }
-            if(mapData.metadata.songAuthorName !== "") { entry.setSongauthorname(mapData.metadata.songAuthorName); }
-            if(mapData.metadata.levelAuthorName !== "") { entry.setLevelauthorname(mapData.metadata.levelAuthorName); }
-            
-            for(const diff of mapData.versions[0].diffs) {
-                let diffEntry = new MapListSchema.Difficulty();
-                diffEntry.setNjs(diff.njs);
-                diffEntry.setNotes(diff.notes);
-                diffEntry.setCharacteristicname(diff.characteristic);
-                diffEntry.setDifficultyname(diff.difficulty);
-                diffEntry.setMods((diff.cinema ? MapListSchema.MapMods.CINEMA : 0) +
-                    (diff.me ? MapListSchema.MapMods.MAPPINGEXTENSIONS : 0) +
-                    (diff.chroma ? MapListSchema.MapMods.CHROMA : 0) +
-                    (diff.ne ? MapListSchema.MapMods.NOODLEEXTENSIONS : 0) +
-                    (diff.vivify ? MapListSchema.MapMods.VIVIFY : 0));
-                diffEntry.setEnvironmentname(diff.environment.replaceAll("Environment", ""));
-                
-                entry.addDifficulties(diffEntry);
-            }
-            
-            mapListMap.set(mapData.id, entry);
+            updateCacheData(mapData);
             lastMap = mapData;
         }
         
-        console.log(`Cached ${amount} maps (currently at: ${lastMap.id}) ...`);
+        console.log(`[Scraper] Cached ${amount} maps (currently at: ${lastMap.id}) ...`);
         
         if(!data.docs.length) {
-            console.log("No maps left!");
+            console.log("[Scraper] No maps left!");
             keepGoing = false;
         } else {
             currentDateString = lastMap.uploaded;
             await delay(100);
+            keepGoing = false;
         }
     }
     
-    fs.writeFileSync("./cached.proto", mapList.serializeBinary());
+    await saveProtobufCache();
     
     //fs.writeFileSync("./test.json", JSON.stringify(MapListSchema.MapList.deserializeBinary(mapList.serializeBinary()).toObject(), null, "\t"));
 }
-run();
+await initCache();
+
+// https://stackoverflow.com/a/79082701
+async function compressUint8Array(uint8Array) {
+    return new Uint8Array(await new Response(new Blob([uint8Array]).stream().pipeThrough(new CompressionStream('gzip'))).arrayBuffer());
+}
+async function saveProtobufCache() {
+    let saveData = await compressUint8Array(mapList.serializeBinary());
+    await fs.writeFile("./cached.proto.gz", saveData, err => {
+        if (err) {
+            console.error(err);
+        }
+    });
+    
+    try {
+        fs.writeFileSync("./test.json", JSON.stringify(MapListSchema.MapList.deserializeBinary(mapList.serializeBinary()).toObject(), null, "\t"));
+    } catch(err) {
+        console.error(err.message);
+    }
+} 
+
+function updateCacheData(mapData) {
+    let entry = new MapListSchema.MapMetadata();
+    entry.setKey(parseInt(mapData.id, 16));
+    entry.setHash(mapData.versions[0].hash);
+    entry.setDuration(mapData.metadata.duration);
+    entry.setUploaded(Math.round(new Date(mapData.lastPublishedAt).getTime() / 1000));
+    entry.setLastupdated(Math.round(new Date(mapData.updatedAt).getTime() / 1000));
+    entry.setMods((doesMapUseMod(mapData.versions[0], "cinema") ? MapListSchema.MapMods.CINEMA : 0) +
+        (doesMapUseMod(mapData.versions[0], "me") ? MapListSchema.MapMods.MAPPINGEXTENSIONS : 0) +
+        (doesMapUseMod(mapData.versions[0], "chroma") ? MapListSchema.MapMods.CHROMA : 0) +
+        (doesMapUseMod(mapData.versions[0], "ne") ? MapListSchema.MapMods.NOODLEEXTENSIONS : 0) +
+        (doesMapUseMod(mapData.versions[0], "vivify") ? MapListSchema.MapMods.VIVIFY : 0));
+
+    let votes = new MapListSchema.Votes();
+    votes.setUp(mapData.stats.upvotes);
+    votes.setDown(mapData.stats.downvotes);
+    entry.setVotes(votes);
+
+    if("curator" in mapData) { entry.setCuratorname(mapData.curator.name); }
+    if(mapData.metadata.songName !== "") { entry.setSongname(mapData.metadata.songName); }
+    if(mapData.metadata.songSubName !== "") { entry.setSongsubname(mapData.metadata.songSubName); }
+    if(mapData.metadata.songAuthorName !== "") { entry.setSongauthorname(mapData.metadata.songAuthorName); }
+    if(mapData.metadata.levelAuthorName !== "") { entry.setLevelauthorname(mapData.metadata.levelAuthorName); }
+
+    for(const diff of mapData.versions[0].diffs) {
+        let diffEntry = new MapListSchema.Difficulty();
+        diffEntry.setNjs(diff.njs);
+        diffEntry.setNotes(diff.notes);
+        diffEntry.setCharacteristicname(diff.characteristic);
+        diffEntry.setDifficultyname(diff.difficulty);
+        diffEntry.setMods((diff.cinema ? MapListSchema.MapMods.CINEMA : 0) +
+            (diff.me ? MapListSchema.MapMods.MAPPINGEXTENSIONS : 0) +
+            (diff.chroma ? MapListSchema.MapMods.CHROMA : 0) +
+            (diff.ne ? MapListSchema.MapMods.NOODLEEXTENSIONS : 0) +
+            (diff.vivify ? MapListSchema.MapMods.VIVIFY : 0));
+        diffEntry.setEnvironmentname(diff.environment.replaceAll("Environment", ""));
+
+        entry.addDifficulties(diffEntry);
+    }
+
+    mapListMap.set(mapData.id, entry);
+}
+function removeCacheData(mapData) {
+    mapListMap.del(mapData.id);
+}
+
+let socket;
+let beatSaverSocketTimeout;
+function startBeatSaverSocket() {
+    clearTimeout(beatSaverSocketTimeout);
+    console.log("[Socket] Establishing socket connection to BeatSaver...");
+    
+    socket = new WebSocket(BeatSaverSocketURL);
+
+    beatSaverSocketTimeout = setTimeout(() => {
+        socket.removeEventListener("open");
+        socket.removeEventListener("close");
+        socket.removeEventListener("message");
+        
+        console.warn("[Socket] BeatSaver socket connection hit a timeout after 15 seconds, trying again...");
+        startBeatSaverSocket();
+    }, 15000);
+    
+    socket.addEventListener("open", event => {
+        console.log("[Socket] BeatSaver socket connection established");
+        clearTimeout(beatSaverSocketTimeout);
+    });
+    socket.addEventListener("close", event => {
+        console.warn("[Socket] BeatSaver socket connection closed, reconnecting in 15 seconds...");
+        beatSaverSocketTimeout = setTimeout(startBeatSaverSocket, 15000);
+    });
+    
+    socket.addEventListener("message", async event => {
+        const receivedData = JSON.parse(event.data);
+        
+        switch(receivedData.type) {
+            case "MAP_UPDATE":
+                console.log(receivedData);
+                console.log(`[Socket] Updating key ${receivedData.msg.id}`);
+                updateCacheData(receivedData.msg);
+                await saveProtobufCache();
+                break;
+            case "MAP_DELETE":
+                console.log(receivedData);
+                console.log(`[Socket] Deleting key ${receivedData.msg.id}`);
+                removeCacheData(receivedData.msg);
+                await saveProtobufCache();
+                break;
+                
+            default:
+                console.warn(`[Socket] Unhandled event type: ${receivedData.type}`);
+                break;
+        }
+    });
+}
+startBeatSaverSocket();
